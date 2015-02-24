52e4e0a8925ce7312689cc39dee1de11db4d990a
Erik Ihrén <erikihr@gmail.com>
2015-02-24 18:49:44 +0100

    Removed deprecated code from setup_editor.js

--- setup_editor.js
+++ setup_editor.js
@@ -1,40 +1,3 @@
-/*function loadResult () {
-    console.log("Loading");
-    // add error checking ?
-    $.get("inc/read_result.php",
-	  {id: readCookie("session")},
-	  function (data) {
-	      result.setValue(data,1);
-	  });
-};
-
-function runCode (callback) {
-    console.log("Running");
-    // Add some kind of hashing to see if it has changed since last save?
-    $.post("inc/run_code.php",
-	   {id: readCookie("session")},
-	   function(data) {
-	       console.log(data);
-	       if (callback) {
-		   callback();
-	       }
-	   }
-	  );
-    
-};
-
-function saveFile(callback) {
-    console.log("Saving");
-    var contents = editor.getSession().getValue();
-    $.post("inc/compile.php",
-	   {contents: contents,
-	    id: readCookie("session")},
-	   callback
-	  );
-};
-*/
-
-
 function testCode () {
     console.log("Testing");
     for (var i = 0; i < nrTests; i++) {
@@ -44,9 +7,6 @@ function testCode () {
     var message = {event: "test", data: editor.getSession().getValue(), id: readCookie("session")};
     ws.send(JSON.stringify(message));
 };
-
-
-
 var editor = ace.edit("editor");
 var editor_div = document.getElementById('editor');
 var doc = editor.getSession().getDocument()
@@ -69,7 +29,6 @@ editor.commands.addCommand({
     exec: function() {
 	var message = {event: "run",data: editor.getSession().getValue(), id: readCookie("session")};
 	ws.send(JSON.stringify(message));
-	//saveFile(runCode(loadResult));
     }
 });
 editor.commands.addCommand({
