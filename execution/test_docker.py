7beaa95f28d6351da339174ff9fb9ceb52bfc931
Erik Ihrén <erikihr@gmail.com>
2015-02-27 21:31:25 +0100

    Turned off network for docker instances and fixed problem with database commits.

--- execution/test_docker.py
+++ execution/test_docker.py
@@ -13,7 +13,7 @@ partyid = int(sys.argv[4])
 result = ""
 for x in range(1,nrTests+1):
     #print "Running test %d-%d" % (problemid,x)
-    os.system("docker run -v /var/www/crashcompile/execution/%s.txt:/student.py -v /var/www/crashcompile/tests/%d/in%d:/test.txt student_test >/var/www/crashcompile/execution/results_%s.txt 2>&1" % (id,problemid,x,id))
+    os.system("docker run --net none -v /var/www/crashcompile/execution/%s.txt:/student.py -v /var/www/crashcompile/tests/%d/in%d:/test.txt student_test >/var/www/crashcompile/execution/results_%s.txt 2>&1" % (id,problemid,x,id))
     diff = os.popen("diff /var/www/crashcompile/tests/%d/out%d /var/www/crashcompile/execution/results_%s.txt" % (problemid,x,id))
     output = diff.read()
     message = "T#%s#%d#" % (id,x)
