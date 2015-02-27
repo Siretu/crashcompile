a5eb90374e97be6b1094350a7560a0af9e654601
Erik Ihrén <erikihr@gmail.com>
2015-02-27 21:35:17 +0100

    Removed deprecated files.

--- execution/run_docker.sh
+++
@@ -1,2 +0,0 @@
-#rm /var/wwwstudent_results.txt
-docker run --net none --volume /var/www/crashcompile/execution/$1.txt:/student.py student_test python /student.py >/var/www/crashcompile/execution/results_$1.txt 2>&1
